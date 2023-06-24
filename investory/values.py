f3c3bf7fa657a424a72e4af09694e105fb1e8403
Nasser Alkmim <nasser.alkmim@gmail.com>
2023-06-24 10:32:00 +0200

    Add value module

---
+++ investory/values.py
@@ -0,0 +1,10 @@
+"""Get the commodities value from the internet.
+
+The commodities are form the ledger file and the value is obtained for the end of each
+month.  Each commodity values are saved to their ledger file with the 'P directive'.
+
+Usage:
+
+    $ python -m investory.values -f <ledger>
+
+"""
