d856c881631d3fa20444929c2fd1a49093fbf16e
Nasser Alkmim <nasser.alkmim@gmail.com>
2023-06-27 10:29:20 +0200

    Add functions for one commodity only

--- investory/values.py
+++ investory/values.py
@@ -2,15 +2,18 @@
 
 Usage:
 
-    $ python -m investory.values commodities.csv
+    $ python -m investory.values <commodity ticker>
 
-The commodities file can be obtained for instance,
+The commodities list can be obtained from a ledger file:
 
     $ hledger -f all.ledger commodities > commodities.csv
 
+Some commodities tickers need a specific suffix according to Yahoo database.
+For example, Brazilian stocks need a '.SA'.
 """
 import numpy as np
 import pandas as pd
+import os
 import yahooquery as yq
 import datetime
 from dataclasses import dataclass, field
@@ -19,23 +22,26 @@ from dataclasses import dataclass, field
 @dataclass
 class Commodity:
     ticker: str = ""
-    date: list[datetime.date] = field(default_factory=list)
+    yahoo_name: str = ticker
+    date: list[str] = field(default_factory=list)
     price: list[float] = field(default_factory=list)
 
-
-def collect_commodities(file: str) -> list[Commodity]:
-    """Collect the commodities from the csv file."""
-    tickers_list = np.loadtxt(file, dtype=str)
-    commodities = []
-    for ticker in tickers_list:
-        commodities.append(Commodity(ticker=ticker))
-    return commodities
+    if ticker in ["VWCE", "SXR8"]:
+        currency: str = "€"
+    else:
+        currency: str = "$"
 
 
 def get_commodity_price(
     commodity: Commodity, date: datetime.date
-) -> tuple[datetime.date | None, float | None]:
+) -> tuple[str, float]:
     """Getting the commodity price on specific date."""
+
+    # if ticker end with number, it is a Brazilian stock, which has a
+    # ".SA" suffix
+    if commodity.ticker[-1] in ["3", "4", "11", "5", "31"]:
+        commodity.yahoo_name = f"{commodity.ticker}.SA"
+        commodity.currency = "R$"
 
     # get history price for the next 10 days
     data = yq.Ticker(commodity.ticker).history(
@@ -45,38 +51,49 @@ def get_commodity_price(
     # extract just the first valid date and close value
     try:
         # get string for datetime object
-        date = data.index[0][1].strftime("%Y-%m-%d")
-        price = data.close[0]
+        date_string = data.index[0][1].strftime("%Y-%m-%d")
+        value = data.close[0]
+        print("Found value ", value, "at ", date_string)
     except IndexError:
         # if after 10 days there still no data, it is probably not available
-        price=None
-        date=None
+        value = np.NaN
+        date_string = ""
 
-    return date, price
+    return date_string, value
+
+
+def get_last_date_recorded(commodity: str) -> datetime.date:
+    """Get the last date recorded in the file."""
+    return datetime.date(2023, 1, 1)
+
+
+def get_initial_date(commodity: str) -> datetime.date:
+    """Get the date from which to obtain the commodity values."""
+    if os.path.exists(f"{commodity}.ledger"):
+        last_date_recorded = get_last_date_recorded(commodity)
+        return last_date_recorded
+    else:
+        # if file does not exist start from this date
+        return datetime.date(2017, 1, 1)
 
 
 if __name__ == "__main__":
     import argparse
 
-    parser = argparse.ArgumentParser(description="Process multiple file paths")
+    parser = argparse.ArgumentParser(description="Get value of commodity")
     parser.add_argument(
-        "file_paths", metavar="FILE_PATHS", nargs="+", help="File paths to process"
+        "commodity", metavar="STRING", nargs="+", help="Ticker form Yahoo database"
     )
     args = parser.parse_args()
 
-    file = args.file_paths[0]
+    commodity = Commodity(ticker=args.commodity[0])
 
-    commodities = collect_commodities(file)
+    initial_date = get_initial_date(commodity.ticker)
 
     # loop over month end (business day 'BM') from 2017 until today
     for month_end in pd.date_range(
-        datetime.date(2017, 1, 1), datetime.date(2017, 3, 1), freq="BM"
+        initial_date, datetime.date(2017, 3, 1), freq="BM"
     ):
-        # loop over each commodity and add the price and date to their list
-        for commodity in commodities:
-            date, price = get_commodity_price(commodity, month_end)
-            commodity.date.append(date)
-            commodity.price.append(price)
-
-
-    print(commodities[0])
+        date, value = get_commodity_price(commodity, month_end)
+        with open(f"{commodity.ticker}.ledger", "w") as f:
+            f.write(f"P {date} {commodity.ticker} {commodity.currency} {value}")
