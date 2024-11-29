171919239cf4fde7655b7f8c501e8a264d402de4
Nasser Alkmim <nasser.alkmim@gmail.com>
2024-11-29 11:00:21 +0100

    [WIP] add summary report

--- investory/reports.py
+++ investory/reports.py
@@ -17,13 +17,6 @@ figtex.style()
 DATA = "/home/nasser/Sync/documents/admin/finances/data"
 
 
-def clean_currency(value):
-    """Clean currency value in a string."""
-    if isinstance(value, str):
-        return value.replace('R$', '')
-    return value
-
-
 def generate_asset_distribution_graph(period: int, ledger: str) -> None:
     """Generate pie plot for asset distribution."""
     command: list[str] = [
@@ -48,9 +41,10 @@ def generate_asset_distribution_graph(period: int, ledger: str) -> None:
     output, _ = process.communicate()
     csv_data: io.StringIO = io.StringIO(output)
     df: pd.DataFrame = pd.read_csv(csv_data)
-    df["balance"] = df["balance"].apply(clean_currency).astype(float)
+    df = df.replace("R\\$", "", regex=True)
+    df["balance"] = df["balance"].apply(pd.to_numeric)
 
-    fig, ax = plt.subplots(figsize=(4, 4))
+    fig, ax = plt.subplots(figsize=(3, 3))
     if df.empty:
         ax.set_xlim(0, 1)
         ax.set_ylim(0, 1)
@@ -63,7 +57,54 @@ def generate_asset_distribution_graph(period: int, ledger: str) -> None:
                     ax=ax)
         plt.title('Asset Distribution')
     fig.savefig(f"reports/{period}/asset-distribution.svg",
-                bbox_inches="tight")
+                bbox_inches="tight", transparent=True)
+
+
+def generate_asset_evolution_graph(period: int, ledger: str) -> None:
+    """Generate pie plot for asset distribution."""
+
+    command: list[str] = [
+        "hledger",
+        "-f", f"{ledger}",
+        "-f", f"{DATA}/prices/BRLUSD=X.ledger",
+        "-f", f"{DATA}/prices/EURUSD=X.ledger",
+        "bal", "acct:^assets:investments",
+        "--historical", "--monthly",
+        "--drop", "2",
+        "--depth", "3",
+        "--value=end,R$",
+        "--no-total",
+        "--infer-market-prices",
+    ]
+
+    if period != 0:
+        command.extend(["--end", f"{period + 1}"])
+
+    process = subprocess.Popen(command,
+                               stdout=subprocess.PIPE,
+                               shell=False,
+                               universal_newlines=True)
+    output, _ = process.communicate()
+    csv_data: io.StringIO = io.StringIO(output)
+    df: pd.DataFrame = pd.read_csv(csv_data, index_col=0)
+    df = df.replace("R\\$", "", regex=True)
+    df = df[df.columns].apply(pd.to_numeric)
+    # convert columns name to datetime
+    df.columns = pd.to_datetime(df.columns, format="%Y-%m")
+    df = df.transpose()
+
+    fig, ax = plt.subplots(figsize=(8, 3))
+    if df.empty:
+        ax.set_xlim(0, 1)
+        ax.set_ylim(0, 1)
+        ax.text(0.5, 0.5, 'No data available',
+                ha='center', va='center', fontsize=12)
+        ax.axis('off')  # Hide axes
+    else:
+        df.plot.area(ax=ax)
+        plt.title('Asset Evolution')
+    fig.savefig(f"reports/{period}/asset-evolution.svg",
+                bbox_inches="tight", transparent=True)
 
 
 def run_command(command: str) -> str:
@@ -73,30 +114,31 @@ def run_command(command: str) -> str:
     return result.stdout.strip()
 
 
-def generate_report(period: int, ledger: str):
+def generate_yearly_report(period: int, ledger: str, currency: str = "€"):
     os.makedirs(f"reports/{period}", exist_ok=True)
 
     generate_asset_distribution_graph(period, ledger)
+    generate_asset_evolution_graph(period, ledger)
 
     commands = [
         # Income statement
         f"echo -en '* Monthly income graph\n[[file:monthly-in-{period}.svg]]\n' > reports/{period}/is-{period}.org",
         f"echo -en '* Summary income statement\n' >> reports/{period}/is-{period}.org",
-        f"hledger -f {ledger} is --sort --depth 1 --period 'from {period - 2} to {period + 1}' --tree --pretty >> reports/{period}/is-{period}.org",
+        f"hledger -f {ledger} is --sort --depth 1 --period 'from {period - 2} to {period + 1}' --tree --pretty=no >> reports/{period}/is-{period}.org",
         f"echo -en '* Income statement\n' >> reports/{period}/is-{period}.org",
-        f"hledger -f {ledger} is --sort --depth 2 --monthly --average --period {period} --tree --pretty >> reports/{period}/is-{period}.org",
+        f"hledger -f {ledger} is --sort --depth 2 --monthly --average --period {period} --tree --pretty=no >> reports/{period}/is-{period}.org",
         f"echo -en '* Full Income statement\n' >> reports/{period}/is-{period}.org",
-        f"hledger -f {ledger} is --sort --yearly --period {period} --tree --pretty --layout tall >> reports/{period}/is-{period}.org",
+        f"hledger -f {ledger} is --sort --yearly --period {period} --tree --pretty=no --layout tall >> reports/{period}/is-{period}.org",
         f"echo -en '* Full Income statement monthly\n' >> reports/{period}/is-{period}.org",
-        f"hledger -f {ledger} is --sort --monthly --average --row-total --period {period} --tree --pretty --layout tall >> reports/{period}/is-{period}.org",
+        f"hledger -f {ledger} is --sort --monthly --average --row-total --period {period} --tree --pretty=no --layout tall >> reports/{period}/is-{period}.org",
         # Balance sheet
-        f"echo -en '* Monthly investments evolution graph\n[[file:investments-{period}.svg]] [[file:asset-distribution.svg]]\n' > reports/{period}/bs-{period}.org",
+        f"echo -en '* Monthly investments evolution graph\n[[file:asset-evolution.svg]] [[file:asset-distribution.svg]]\n' > reports/{period}/bs-{period}.org",
         f"echo -en '* Summary balance sheet last three years\n' >> reports/{period}/bs-{period}.org",
-        f"hledger -f {ledger} bs --tree --pretty --depth 1 --alias '/^(income|expenses)\b/=equity:retained earnings' --period 'from {period - 2} to {period + 1}' --infer-market-prices --value=end,€ -f {DATA}/prices/EURUSD=X.ledger -f {DATA}/prices/BRLUSD=X.ledger --yearly >> reports/{period}/bs-{period}.org",
+        f"hledger -f {ledger} bs --tree --pretty=no --depth 1 --alias '/^(income|expenses)\b/=equity:retained earnings' --period 'from {period - 2} to {period + 1}' --infer-market-prices --value=end,{currency} -f {DATA}/prices/EURUSD=X.ledger -f {DATA}/prices/BRLUSD=X.ledger --yearly >> reports/{period}/bs-{period}.org",
         f"echo -en '* Balance sheet valued at period ends\n' >> reports/{period}/bs-{period}.org",
-        f"hledger -f {ledger} bs --depth 3 --infer-market-prices --value=end --tree --pretty --no-total --period {period} >> reports/{period}/bs-{period}.org",
+        f"hledger -f {ledger} bs --depth 3 --infer-market-prices --value=end --tree --pretty=no --no-total --period {period} >> reports/{period}/bs-{period}.org",
         f"echo -en '* Investments converted to cost in R$\n' >> reports/{period}/bs-{period}.org",
-        f"hledger -f {ledger} bal type:AL --historical investments --period {period} --layout tall --tree --pretty --drop 5 --depth 5 --no-total >> reports/{period}/bs1-{period}.org",
+        f"hledger -f {ledger} bal type:AL --historical investments --period {period} --layout tall --tree --pretty=no --drop 5 --depth 5 --no-total >> reports/{period}/bs1-{period}.org",
         f"hledger -f {ledger} bal type:AL --historical investments -f {DATA}/prices/EURUSD=X.ledger SD.ledger -f {DATA}/prices/BRLUSD=X.ledger --period {period} --infer-equity --cost --infer-cost --infer-market-prices --exchange=R$ --drop 3 | grep -v '                   0' >> reports/{period}/bs2-{period}.org",
         f"echo -en 'Investments {period}, converted to cost in R$ \n' >> reports/{period}/bs-{period}.org",
         f"paste reports/{period}/bs1-{period}.org reports/{period}/bs2-{period}.org | column -s $'\t' -t >> reports/{period}/bs-{period}.org",
@@ -106,7 +148,23 @@ def generate_report(period: int, ledger: str):
     for command in commands:
         run_command(command)
 
-    print(f"Completed report for period: {period}")
+        print(f"Completed report for period: {period}")
+
+
+def generate_summary_report(ledger: str, currency: str = "€"):
+    generate_asset_distribution_graph(0, ledger)
+    generate_asset_evolution_graph(0, ledger)
+    commands = [
+        # Balance sheet
+        "echo -en '* Monthly investments evolution graph\n[[file:asset-evolution.svg]] [[file:asset-distribution.svg]]\n' > reports/summary.org",
+        "echo -en '* Summary balance sheet last three years\n' >> reports/summary.org",
+        f"hledger -f {ledger} bs --tree --pretty=no --depth 1 --alias '/^(income|expenses)\b/=equity:retained earnings' --period 'from 3 years ago to today' --infer-market-prices --value=end,{currency} -f {DATA}/prices/EURUSD=X.ledger -f {DATA}/prices/BRLUSD=X.ledger --yearly >> reports/summary.org",
+    ]
+
+    for command in commands:
+        run_command(command)
+
+        print("Completed summary report")
 
 
 def get_ledger_years(ledger_file: str) -> List[int]:
@@ -129,12 +187,20 @@ if __name__ == "__main__":
     parser.add_argument(
         "--ledger", help="Ledger file", required=False, type=str, default="all.ledger"
     )
+    parser.add_argument(
+        "--currency", help="Currency", required=False, type=str, default="€"
+    )
     args = parser.parse_args()
     periods: list[int] = get_ledger_years(args.ledger)
 
     # Each process (CPU) runs the function for a period simultaneously
     with ProcessPoolExecutor(max_workers=len(periods)) as executor:
-        futures: List[Future] = [executor.submit(generate_report, period, args.ledger) for period in periods]
+        futures: List[Future] = [
+            executor.submit(generate_yearly_report, period, args.ledger, args.currency)
+            for period in periods
+        ]
+        futures.append(executor.submit(generate_summary_report, args.ledger, args.currency))
+
         for future in as_completed(futures):
             future.result()
 
